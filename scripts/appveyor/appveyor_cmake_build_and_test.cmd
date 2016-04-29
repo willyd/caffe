578240826b51d6edaaf2b73789c185d57261c30c
Guillaume Dumont <dumont.guillaume@gmail.com>
2016-04-29 17:01:00 -0400

    Added -y option to conda install on cmake appveyor build

--- scripts/appveyor/appveyor_cmake_build_and_test.cmd
+++ scripts/appveyor/appveyor_cmake_build_and_test.cmd
@@ -3,7 +3,7 @@
 :: Set python 2.7 with conda as the default python
 set PATH=C:\Miniconda-x64;C:\Miniconda-x64\Scripts;C:\Miniconda-x64\Library\bin;%PATH%
 python --version
-conda install numpy
+conda install -y numpy
 
 :: Download the ninja build tool
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/willyd/caffe-builder/master/download_ninja.ps1'))"
