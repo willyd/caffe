e091b230729a83328f9ebd6392c15d11fa42e6b3
Guillaume Dumont <dumont.guillaume@gmail.com>
2017-12-19 20:39:08 -0500

    Fix python build

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -176,12 +176,22 @@ if(BUILD_python)
     find_package(Boost 1.46 COMPONENTS "python-py${boost_py_version}")
     set(Boost_PYTHON_FOUND ${Boost_PYTHON-PY${boost_py_version}_FOUND})
 
+    if(NOT Boost_PYTHON_FOUND)
+      find_package(Boost 1.46 COMPONENTS "python${boost_py_version}")
+      set(Boost_PYTHON_FOUND ${Boost_PYTHON${boost_py_version}_FOUND})
+    endif()
+
     while(NOT "${version}" STREQUAL "" AND NOT Boost_PYTHON_FOUND)
       STRING( REGEX REPLACE "([0-9.]+).[0-9]+" "\\1" version ${version} )
 
       STRING( REGEX REPLACE "[^0-9]" "" boost_py_version ${version} )
       find_package(Boost 1.46 COMPONENTS "python-py${boost_py_version}")
       set(Boost_PYTHON_FOUND ${Boost_PYTHON-PY${boost_py_version}_FOUND})
+
+      if(NOT Boost_PYTHON_FOUND)
+        find_package(Boost 1.46 COMPONENTS "python${boost_py_version}")
+        set(Boost_PYTHON_FOUND ${Boost_PYTHON${boost_py_version}_FOUND})
+      endif()
 
       STRING( REGEX MATCHALL "([0-9.]+).[0-9]+" has_more_version ${version} )
       if("${has_more_version}" STREQUAL "")
