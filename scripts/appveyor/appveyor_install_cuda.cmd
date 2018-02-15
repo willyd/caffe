08b6ece6246dc0a11fbfd9a3404d425031267e5b
Guillaume Dumont <dumont.guillaume@gmail.com>
2018-02-14 19:41:56 -0500

    Enabled CUDA on CI

--- scripts/appveyor/appveyor_install_cuda.cmd
+++ scripts/appveyor/appveyor_install_cuda.cmd
@@ -1,14 +1,14 @@
 @echo off
-echo Downloading CUDA toolkit 8 ...
-appveyor DownloadFile  https://developer.nvidia.com/compute/cuda/8.0/prod/local_installers/cuda_8.0.44_windows-exe -FileName cuda_8.0.44_windows.exe
-echo Installing CUDA toolkit 8 ...
-cuda_8.0.44_windows.exe -s compiler_8.0 ^
-                           cublas_8.0 ^
-                           cublas_dev_8.0 ^
-                           cudart_8.0 ^
-                           curand_8.0 ^
-                           curand_dev_8.0 ^
-                           nvml_dev_8.0
+echo Downloading CUDA toolkit 9.1 ...
+appveyor DownloadFile  https://developer.nvidia.com/compute/cuda/9.1/Prod/local_installers/cuda_9.1.85_win10 -FileName cuda_9.1.85_win10.exe
+echo Installing CUDA toolkit 9.1 ...
+cuda_9.1.85_win10.exe -s nvcc_9.1 ^
+                         cublas_9.1 ^
+                         cublas_dev_9.1 ^
+                         cudart_9.1 ^
+                         curand_9.1 ^
+                         curand_dev_9.1 ^
+                         nvml_dev_9.1
 :: Add CUDA toolkit to PATH
-set PATH=%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v8.0\bin;%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v8.0\libnvvp;%PATH%
+set PATH=%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v9.1\bin;%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v9.1\libnvvp;%PATH%
 nvcc -V
